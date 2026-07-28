41acbfe3609a1b79dcac6e22f0fc557b294328bc
LeGreffier <261968324+legreffier[bot]@users.noreply.github.com>
2026-07-28 17:17:57 +0200

    chore(openapi): refresh task authority validators
    MoltNet-Diary: 637bc8d1-0187-4f1c-ad48-06ae8a90e934
    
    Task-Group: issue-1348-task-authority
    
    Task-Family: bugfix
    
    Task-Completes: true

--- libs/moltnet-api-client/oas_cfg_gen.go
+++ libs/moltnet-api-client/oas_cfg_gen.go
@@ -18,7 +18,8 @@ import (
 )
 
 var regexMap = map[string]ogenregex.Regexp{
-	"\\S": ogenregex.MustCompile("\\S"),
+	"\\S":                                    ogenregex.MustCompile("\\S"),
+	"^(?!.*[\\r\\n])[a-zA-Z0-9_.:-]{1,128}$": ogenregex.MustCompile("^(?!.*[\\r\\n])[a-zA-Z0-9_.:-]{1,128}$"),
 	"^(?:\\*\\.)?(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)(?:\\.(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?))*$": ogenregex.MustCompile("^(?:\\*\\.)?(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)(?:\\.(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?))*$"),
 	"^[0-9]+[KMGTP]?$": ogenregex.MustCompile("^[0-9]+[KMGTP]?$"),
 	"^[0-9]+[KMG]?$":   ogenregex.MustCompile("^[0-9]+[KMG]?$"),
@@ -34,7 +35,6 @@ var regexMap = map[string]ogenregex.Regexp{
 	"^[a-zA-Z0-9][a-zA-Z0-9._:-]{0,199}$": ogenregex.MustCompile("^[a-zA-Z0-9][a-zA-Z0-9._:-]{0,199}$"),
 	"^[a-zA-Z0-9][a-zA-Z0-9_-]{0,99}$":    ogenregex.MustCompile("^[a-zA-Z0-9][a-zA-Z0-9_-]{0,99}$"),
 	"^[a-zA-Z0-9_-]+$":                    ogenregex.MustCompile("^[a-zA-Z0-9_-]+$"),
-	"^[a-zA-Z0-9_.:-]{1,128}$":            ogenregex.MustCompile("^[a-zA-Z0-9_.:-]{1,128}$"),
 	"^bafk[a-z2-7]+$":                     ogenregex.MustCompile("^bafk[a-z2-7]+$"),
 	"^ed25519:[A-Za-z0-9+/=]+$":           ogenregex.MustCompile("^ed25519:[A-Za-z0-9+/=]+$"),
 	"^sha256:[0-9a-f]{64}$":               ogenregex.MustCompile("^sha256:[0-9a-f]{64}$"),
